0635589cc36f9d3e279096a6db39f82559e325c1
Bit Handshake <adam@monotech.hu>
2024-02-13 01:27:39 +0100

    0.0.1.5

--- source-code/cljs/lazy_loader/api.cljs
+++ source-code/cljs/lazy_loader/api.cljs
@@ -10,13 +10,13 @@
 
 ; @tutorial Demo
 ;
-; @code Image sensor
+; @--- Image sensor
 ; (defn my-image-sensor
 ;   []
 ;   [image-sensor :my-loader {:get-element-f (fn [_] (.getElementById js/document "my-element"))
 ;                             :uri "/my-image.png"}])
 ;
-; @code Image element
+; @--- Image element
 ; (defn my-ui
 ;   []
 ;   [:<> [:div {:id "my-offset" :style {:height :100vh :width :100%}}]
